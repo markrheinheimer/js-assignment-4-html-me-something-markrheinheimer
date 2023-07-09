52e2c65236a1f36fa3e217be677331f46bcdcd00
[Mark Rheinheimer] <markrheinheimer@protonmail.comgit config --global user.name markrheinheimer>
2023-07-08 21:06:12 -0500

    added more styles

---
+++ script.js
@@ -0,0 +1,74 @@
+const mazda626 = document.getElementById('car1');
+const mazda626show = document.querySelector('.mazda626');
+const mazda323 = document.getElementById('car2');
+const mazda323show = document.querySelector('.mazda323');
+const mazda626a = document.getElementById('car3');
+const mazda626ashow = document.querySelector('.mazda626a');
+const dodgeram = document.getElementById('car4');
+const dodgeramshow = document.querySelector('.dodgeram');
+const fordexpedition = document.getElementById('car5');
+const fordexpeditionshow = document.querySelector('.fordexpedition');
+const fordexplorer = document.getElementById('car6');
+const fordexplorershow = document.querySelector('.fordexplorer');
+const minicooper = document.getElementById('car7');
+const minicoopershow = document.querySelector('.minicooper');
+const fordtransit = document.getElementById('car8');
+const fordtransitshow = document.querySelector('.fordtransit');
+
+//Event Listeners
+mazda626.addEventListener('click', (e) => {
+  mazda626show.classList.remove('noshow');
+  setTimeout(
+    () => window.scrollTo({ top: 0, left: 0, behavior: 'smooth' }),
+    5000
+  );
+});
+mazda323.addEventListener('click', (e) => {
+  mazda323show.classList.remove('noshow');
+  setTimeout(
+    () => window.scrollTo({ top: 0, left: 0, behavior: 'smooth' }),
+    5000
+  );
+});
+mazda626a.addEventListener('click', (e) => {
+  mazda626ashow.classList.remove('noshow');
+  setTimeout(
+    () => window.scrollTo({ top: 0, left: 0, behavior: 'smooth' }),
+    5000
+  );
+});
+dodgeram.addEventListener('click', (e) => {
+  dodgeramshow.classList.remove('noshow');
+  setTimeout(
+    () => window.scrollTo({ top: 0, left: 0, behavior: 'smooth' }),
+    5000
+  );
+});
+fordexpedition.addEventListener('click', (e) => {
+  fordexpeditionshow.classList.remove('noshow');
+  setTimeout(
+    () => window.scrollTo({ top: 0, left: 0, behavior: 'smooth' }),
+    5000
+  );
+});
+fordexplorer.addEventListener('click', (e) => {
+  fordexplorershow.classList.remove('noshow');
+  setTimeout(
+    () => window.scrollTo({ top: 0, left: 0, behavior: 'smooth' }),
+    5000
+  );
+});
+minicooper.addEventListener('click', (e) => {
+  minicoopershow.classList.remove('noshow');
+  setTimeout(
+    () => window.scrollTo({ top: 0, left: 0, behavior: 'smooth' }),
+    5000
+  );
+});
+fordtransit.addEventListener('click', (e) => {
+  fordtransitshow.classList.remove('noshow');
+  setTimeout(
+    () => window.scrollTo({ top: 0, left: 0, behavior: 'smooth' }),
+    5000
+  );
+});
