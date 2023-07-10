0d6bf0ecaaa8c142f94ed385b07b727a1b56d875
[Mark Rheinheimer] <markrheinheimer@protonmail.comgit config --global user.name markrheinheimer>
2023-07-09 21:14:45 -0500

    fixed npm tests

--- spec/grading.spec.js
+++ spec/grading.spec.js
@@ -1,82 +1,89 @@
 // Set up JSDom
 const fs = require('fs');
 const path = require('path');
-const jsdom = require("jsdom");
+const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 
 const options = {
-   runScripts: "dangerously",
-   resources: "usable",
-   pretendToBeVisual: true
-}
+  runScripts: 'dangerously',
+  resources: 'usable',
+  pretendToBeVisual: true,
+};
 
-let html = fs.readFileSync(path.resolve(__dirname, "../index.html"), 'utf8');
-let css = fs.readFileSync(path.resolve(__dirname, "../styles.css"), 'utf8')
+let html = fs.readFileSync(path.resolve(__dirname, '../index.html'), 'utf8');
+let css = fs.readFileSync(path.resolve(__dirname, '../styles.css'), 'utf8');
 
-describe ("Grading Tests: ", function () {
+describe('Grading Tests: ', function () {
+  let window, container;
 
-   let window, container;
+  beforeAll(function () {
+    const dom = new JSDOM(html, options);
+    window = dom.window;
 
-   beforeAll(function() {
+    let stylesElement = window.document.createElement('style');
+    stylesElement.textContent = css;
+    window.document.head.appendChild(stylesElement);
 
-      const dom = new JSDOM(html, options);
-      window = dom.window;
+    window.addEventListener('load', function () {
+      container = window.document;
+    });
+  });
 
-      let stylesElement = window.document.createElement("style");
-      stylesElement.textContent = css;
-      window.document.head.appendChild(stylesElement);
+  it('HTML includes the correct number of certain elements', function () {
+    container = window.document;
 
-      window.addEventListener("load", function() {
-         container = window.document;
-      });
-   });
+    let pElements = container.body.getElementsByTagName('P').length;
+    let headerElements = container.body.getElementsByTagName('Header').length;
+    let footerElements = container.body.getElementsByTagName('Footer').length;
+    let mainElements = container.body.getElementsByTagName('Main').length;
+    let articleElements = container.body.getElementsByTagName('Article').length;
+    let imageElements = container.body.getElementsByTagName('img').length;
 
-   it("HTML includes the correct number of certain elements", function() {
-         let pElements = container.body.getElementsByTagName("P").length;
-         let headerElements = container.body.getElementsByTagName("Header").length;
-         let footerElements = container.body.getElementsByTagName("Footer").length;
-         let mainElements = container.body.getElementsByTagName("Main").length;
-         let articleElements = container.body.getElementsByTagName("Article").length;
-         let imageElements = container.body.getElementsByTagName("img").length;
+    expect(pElements).toBeGreaterThanOrEqual(1);
+    expect(headerElements).toBeGreaterThanOrEqual(1);
+    expect(footerElements).toBeGreaterThanOrEqual(1);
+    expect(mainElements).toBeGreaterThanOrEqual(1);
+    expect(articleElements).toBeGreaterThanOrEqual(1);
+    expect(imageElements).toBeGreaterThanOrEqual(1);
+  });
 
-         expect(pElements).toBeGreaterThanOrEqual(1);
-         expect(headerElements).toBeGreaterThanOrEqual(1);
-         expect(footerElements).toBeGreaterThanOrEqual(1);
-         expect(mainElements).toBeGreaterThanOrEqual(1);
-         expect(articleElements).toBeGreaterThanOrEqual(1);
-         expect(imageElements).toBeGreaterThanOrEqual(1);
-   })
+  it('HTML contains correct number of sections', function () {
+    container = window.document;
 
-   it("HTML contains correct number of sections", function() {
-      let childrenElements = container.body.children;
+    let childrenElements = container.body.children;
 
-      expect(childrenElements.length).toBeGreaterThanOrEqual(3);
-      expect(childrenElements.length).toBeLessThanOrEqual(10);
-   })
+    expect(childrenElements.length).toBeGreaterThanOrEqual(3);
+    expect(childrenElements.length).toBeLessThanOrEqual(10);
+  });
 
-   it("HTML includes external CSS script", function() {
-      let linkElement = container.getElementsByTagName("Link");
-      expect(linkElement.item(0).href.includes('styles.css')).toBeTrue();
-   })
+  it('HTML includes external CSS script', function () {
+    container = window.document;
+    let linkElement = container.getElementsByTagName('Link');
+    expect(linkElement.item(0).href.includes('styles.css')).toBeTrue();
+  });
 
-   it("CSS body sets margin and display", function() {
-      expect(window.getComputedStyle(container.body).display).toEqual("block");
-      expect(window.getComputedStyle(container.body).margin).toEqual("8px");
-   })
+  it('CSS body sets margin and display', function () {
+    container = window.document;
+    expect(window.getComputedStyle(container.body).display).toEqual('block');
+    expect(window.getComputedStyle(container.body).margin).toEqual('8px');
+  });
 
-   it("CSS funParagraph class is green", function() {
-      let funParagraphElement = container.getElementById("testP");
-      expect(window.getComputedStyle(funParagraphElement).color).toEqual("green");
-   })
+  it('CSS funParagraph class is green', function () {
+    container = window.document;
+    console.log(container);
+    let funParagraphElement = container.getElementById('testP');
+    expect(window.getComputedStyle(funParagraphElement).color).toEqual('green');
+  });
 
-   it("CSS mainHeading id is red", function() {
-      let headingElement = container.getElementById("mainHeading");
-      expect(window.getComputedStyle(headingElement).color).toEqual("red");
-   })
+  it('CSS mainHeading id is red', function () {
+    container = window.document;
+    let headingElement = container.getElementById('mainHeading');
+    expect(window.getComputedStyle(headingElement).color).toEqual('red');
+  });
 
-   it("HTML includes HTML entities", function() {
-      // Regex pattern: /(&.+;)/ig
-      const regex = /(&.+;)/ig;
-      expect(html.search(regex)).not.toEqual(-1);
-   })
+  it('HTML includes HTML entities', function () {
+    // Regex pattern: /(&.+;)/ig
+    const regex = /(&.+;)/gi;
+    expect(html.search(regex)).not.toEqual(-1);
+  });
 });
